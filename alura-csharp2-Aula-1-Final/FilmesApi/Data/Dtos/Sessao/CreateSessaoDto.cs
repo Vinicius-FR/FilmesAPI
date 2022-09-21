7d981724efda6e73b49792ee8e8da75e68fcc3ca
Domvs <viniciusfr44a@gmail.com>
2022-09-21 09:40:42 -0300

    last commit

---
+++ alura-csharp2-Aula-1-Final/FilmesApi/Data/Dtos/Sessao/CreateSessaoDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace FilmesApi.Data.Dtos.Sessao
+{
+    public class CreateSessaoDto
+    {
+        public int CinemaId { get; set; }
+        public int FilmeId { get; set; }
+        public DateTime HorarioDeEncerramento { get; set;  }
+    }
+}
